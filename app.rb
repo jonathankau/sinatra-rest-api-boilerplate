03abe31a7afe7997f6a722696882fb5405247756
Jonathan Kau <jkau15@gmail.com>
2018-01-23 22:57:50 -0800

    Setup stubbed API.

--- app.rb
+++ app.rb
@@ -1,6 +1,28 @@
-require 'sinatra'
 require 'sinatra/activerecord'
+require 'sinatra/base'
+require 'sinatra/param'
+require 'json'
 
-get '/' do
-  'Hello world!'
+class App < Sinatra::Base
+  helpers Sinatra::Param
+
+  before do
+    content_type :json
+  end
+
+  get '/' do
+    param :sample_param, String, required: false
+
+    { test: 'hello_world' }.to_json
+  end
+
+  get '/submit/:text' do
+    { test: params['text'] }.to_json
+  end
+
+  get '/error' do
+    halt 400, { error: 'not found' }.to_json
+  end
+
+  run! if app_file == $0
 end
