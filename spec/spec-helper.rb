a9857abd313bcb4a6551220fd634574e3e86202b
Jonathan Kau <jkau15@gmail.com>
2018-02-08 16:27:48 -0800

    Dependency cleanup.

--- spec/spec-helper.rb
+++ spec/spec-helper.rb
@@ -1,9 +1,11 @@
 ENV['RACK_ENV'] = 'test'
+
 require 'minitest/autorun'
 require 'rack/test'
 
-Dir.chdir(File.dirname(File.dirname(__FILE__))) do
-  require_relative '../app'
+ROOT_PATH = File.expand_path('../..', __FILE__)
+Dir.chdir(ROOT_PATH) do
+  require File.join(ROOT_PATH, 'app')
 end
 
 include Rack::Test::Methods
